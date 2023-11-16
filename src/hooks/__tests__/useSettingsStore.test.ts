f06f5d5c98eb84416fe50739afdda6818b32d3d1
gustavo <gustavo@tseller.com.br>
2023-11-16 09:26:30 -0300

    useSettingsStore test

---
+++ src/hooks/__tests__/useSettingsStore.test.ts
@@ -0,0 +1,30 @@
+import { act, renderHook } from "@testing-library/react";
+import { useSettingsStore } from "../useSettingsStore";
+
+describe("useSettingsStore hook", () => {
+  it("should start with default settings", () => {
+    const { result } = renderHook(() => useSettingsStore());
+    expect(result.current.units).toBe("Metric");
+    expect(result.current.timeFormat).toBe("24h");
+  });
+
+  it("should allow changing time format", () => {
+    const { result } = renderHook(() => useSettingsStore());
+
+    act(() => {
+      result.current.setTimeFormat("AM/PM");
+    });
+
+    expect(result.current.timeFormat).toBe("AM/PM");
+  });
+
+  it("should allow changing units", () => {
+    const { result } = renderHook(() => useSettingsStore());
+
+    act(() => {
+      result.current.setUnits("Imperial");
+    });
+
+    expect(result.current.units).toBe("Imperial");
+  });
+});
